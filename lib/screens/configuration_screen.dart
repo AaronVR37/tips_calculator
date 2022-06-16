3a3464a206911bc0e2302744f192f8aac22513f9
AaronVR37 <84838779+AaronVR37@users.noreply.github.com>
2022-06-16 00:26:27 -0500

    Initial commit

---
+++ lib/screens/configuration_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ConfigurationScreen extends StatelessWidget{
+  const ConfigurationScreen({Key? key}) : super(key: key);
+
+  void setDefaultValues(double value) async {
+	  final prefs = await SharedPreferences.getInstance();
+	  await prefs.setDouble('percent', value);
+  }
+
+  Future<double?>getDefaultValues() async {
+	  final prefs = await SharedPreferences.getInstance();
+	  final double percent = prefs.getDouble('precent')??15;
+
+	return percent;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final controllerAmount = TextEditingController();
+    final controllerPercent = TextEditingController();
+    double tip = 0;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Configurations"),
+      backgroundColor: const Color.fromARGB(255, 131, 36, 178),
+      ),
+      body: Center(
+        child:
+        ElevatedButton(
+          onPressed: () {
+            setState((){
+	                double amount = double.parse(controllerAmount.text);
+	                double percent = double.parse(controllerPercent.text);
+                  setDefaultValues(percent);
+
+	                tip = amount * percent / 100;
+	                //setDefaultValues(percent);
+	              });
+          },
+          child:const Text('Go back to the '),
+        ),
+      ),
+    );
+  }
+}
+
+void setState(Null Function() param0) {
+}
